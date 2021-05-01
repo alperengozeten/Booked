68243b45feaf78bfde2cd1076a376aaa3e0de3f8
Berkay Akkus <berkayakkus00@gmail.com>
2021-05-01 16:54:33 +0300

    comments eklendi

--- app/src/main/java/com/example/booked/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/booked/ForgotPasswordActivity.java
@@ -15,14 +15,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+/**
+ * A class for the Forgot Password Screen
+ *
+ * @author NoExpection
+ * @version 2021 Spring
+ */
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    // Properties
     EditText mEmail;
     Button mSendResetEmailBtn;
     Button mGoBackToLoginBtn;
 
     FirebaseAuth mAuth;
 
+    /**
+     * This method sets the activity on create by overriding AppCompatActivity's onCreate method.
+     *
+     * @param savedInstanceState - Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +46,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        // Sets ClickListener object for Back to Login button.
         mGoBackToLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,26 +55,48 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             }
         });
 
+        // Sets ClickListener object for Send Password Reset Link button.
         mSendResetEmailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
 
+                // if the email field is empty, show an error message.
                 if (email.isEmpty() ) {
                     mEmail.setError("Please enter your email address");
                     mEmail.requestFocus();
                     return;
                 }
+                else {
+                    mEmail.setError( null);
+                }
 
+                // if the email address is not valid, show an error message.
                 if ( !Patterns.EMAIL_ADDRESS.matcher(email).matches() ) {
                     mEmail.setError("Please provide a valid email address!");
                     mEmail.requestFocus();
                     return;
                 }
+                else {
+                    mEmail.setError( null);
+                }
 
+                // if it is not an edu.tr type email address, show an error
+                if (!email.substring(email.indexOf("@")).contains(".edu.tr"))
+                {
+                    mEmail.setError("This is not an email address with edu.tr extension");
+                    mEmail.requestFocus();
+                    return;
+                }
+                else {
+                    mEmail.setError( null);
+                }
+
+                // if the account exists, it sends a password reset link to given email.
                 mAuth.sendPasswordResetEmail( email).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
+                        // if the sending reset email task is successful, directs user to login page.
                         if (task.isSuccessful()) {
                             Toast.makeText(ForgotPasswordActivity.this, "Password reset link has been sent to your email address: " + email, Toast.LENGTH_LONG).show();
                             startActivity( new Intent(getApplicationContext(), LoginActivity.class));
@@ -69,6 +104,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
                         }
                         else{
+                            // otherwise, show an error message.
                             Toast.makeText(ForgotPasswordActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
                     }
