ed0cb823f2ba98892223be48be603c289ab36a6a
HP <alperengozeten@gmail.com>
2021-05-01 18:53:42 +0300

    commenting-small changes with edit profile

--- app/src/main/java/com/example/booked/Adapter/MyPostAdapter.java
+++ app/src/main/java/com/example/booked/Adapter/MyPostAdapter.java
@@ -26,20 +26,34 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
+/**
+ * This is the adapter class for the recycler view in the MyPosts page
+ */
 public class MyPostAdapter extends androidx.recyclerview.widget.RecyclerView.Adapter<MyPostAdapter.PostViewHolder> {
 
-    //ArrayList<String> nam;
     ArrayList<Post> posts;
     Context context;
 
+    /**
+     * This constructor takes list of posts and also a reference to the MyPost page as a Context object
+     * @param posts
+     * @param context
+     */
     public MyPostAdapter(ArrayList<Post> posts, Context context) {
         this.posts = posts;
         this.context = context;
     }
 
+    /**
+     * This method creates a PostViewHolder object which holds references to the gui (view) elements
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @NonNull
     @Override
     public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the view from the layout
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_post, parent, false);
 
         PostViewHolder holder = new PostViewHolder(view);
@@ -47,26 +61,35 @@ public class MyPostAdapter extends androidx.recyclerview.widget.RecyclerView.Ada
         return holder;
     }
 
+    /**
+     * This method is called when binding rows (elements)
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
+        // Fil the textviews with the related information
         holder.postDescriptionTextView.setText(posts.get(position).getTitle().toString());
         holder.postPriceTextView.setText(String.valueOf(posts.get(position).getPrice()) + " ₺");
 
-        //Toast.makeText(context,"path:" + Booked.getExamplePath(), Toast.LENGTH_LONG).show();
+        // If the post has a picture, then load it to ImageView
         if ( ! posts.get(position).getPicture().equals("")  ) {
             Picasso.get().load(posts.get(position).getPicture()).fit().centerCrop().into(holder.postPictureImageView);
         }
 
+        // If the delete button is clicked, remove it
         holder.deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // Delete post from the database using the deletePost() method of the Global class
                 Booked.deletePost(posts.get(position));
                 posts.remove(position);
                 notifyDataSetChanged();
             }
         });
 
+        // When the item is clicked, it will lead to the Post page of that post
         holder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,9 +100,11 @@ public class MyPostAdapter extends androidx.recyclerview.widget.RecyclerView.Ada
             }
         });
 
+        // When the post edit button is clicked, it will direct to the EditPost page
         holder.postEditBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Before calling the intent, set the currentPost of the global class to the current row item
                 Booked book = new Booked();
                 book.setCurrentPost(posts.get(position));
                 Intent intent = new Intent(context, EditPost.class);
@@ -88,13 +113,18 @@ public class MyPostAdapter extends androidx.recyclerview.widget.RecyclerView.Ada
         });
     }
 
-
+    /**
+     * This method returns the number of elements(rows)
+     * @return
+     */
     @Override
     public int getItemCount() {
         return posts.size();
     }
 
-
+    /**
+     * This is an inner class that holds references to the views (gui)
+     */
     public class PostViewHolder extends RecyclerView.ViewHolder {
         ImageView postPictureImageView;
         TextView postDescriptionTextView;
@@ -106,13 +136,14 @@ public class MyPostAdapter extends androidx.recyclerview.widget.RecyclerView.Ada
 
         public PostViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            // Initialize the view elements
             deleteButton = itemView.findViewById(R.id.deleteMyPost);
             postPictureImageView = itemView.findViewById(R.id.postPictureImageView);
             postDescriptionTextView = itemView.findViewById(R.id.postDescriptionTextView);
             postPriceTextView = itemView.findViewById(R.id.postPriceTextView);
             postEditBtn = itemView.findViewById(R.id.postEditBtn);
             parentLayout = itemView.findViewById(R.id.postLayout);
-
         }
     }
 }
