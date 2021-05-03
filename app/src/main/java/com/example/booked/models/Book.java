c55712618585de0fdafec7715aedf6aecf3a62a6
YarkinKurt <yarkin.kurt@ug.bilkent.edu.tr>
2021-05-03 16:50:28 +0300

    Added comments on Book, BookProfile model classes

--- app/src/main/java/com/example/booked/models/Book.java
+++ app/src/main/java/com/example/booked/models/Book.java
@@ -1,12 +1,18 @@
 package com.example.booked.models;
 
+/**
+ * This is the model class of Book
+ */
+
 public class Book {
 
+    //Properties
     String bookName;
     String picture;
 
     String id;
 
+    //Constructors
     public Book() {// dont delete this
     }
 
@@ -29,22 +35,45 @@ public class Book {
         this.id = id;
     }
 
+    //Methods
+
+    /**
+     * Set method for book name
+     * @param bookName is the desired book name
+     */
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
 
+    /**
+     * Set method for book's picture
+     * @param picture is the url of the picture
+     */
     public void setPicture(String picture) {
         this.picture = picture;
     }
 
+    /**
+     *Get method for book's name
+     * @return book's name as a string
+     */
     public String getBookName() {
         return bookName;
     }
 
+    /**
+     * Get method for book's picture
+     * @return book's picture's url
+     */
     public String getPicture() {
         return picture;
     }
 
+    /**
+     * Equals method for book object in order to check if two objects are the same book
+     * @param o is an object
+     * @return true if object o and this are the same book
+     */
     @Override
     public boolean equals(Object o) {
 
@@ -61,21 +90,36 @@ public class Book {
 
     }
 
+    /**
+     *
+     * @param id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * ToString method for book object
+     * @return book's name as a string
+     */
     @Override
     public String toString() {
 
         return  getBookName();
     }
 
+    /**
+     * getId method
+     * @return book's id
+     */
     public String getId() {
         return id;
     }
 
-    // function to generate a random string of length n
+    /**
+     * This method generates a random string of length n
+     * @param n is the length of string
+     */
     private void randomKeyGenerator(int n)
     {
 
