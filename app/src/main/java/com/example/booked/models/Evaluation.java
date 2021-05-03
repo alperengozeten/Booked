cecc7eeb355e525a4cecea6fe3360122fcf9ee67
YarkinKurt <yarkin.kurt@ug.bilkent.edu.tr>
2021-05-03 17:40:09 +0300

    Added comments on evaluation, filterable, message

--- app/src/main/java/com/example/booked/models/Evaluation.java
+++ app/src/main/java/com/example/booked/models/Evaluation.java
@@ -1,11 +1,19 @@
 package com.example.booked.models;
 
+
+/**
+ * This is the model class of Evaluation
+ * Evaluation are made by users, to make comments and give a rating to a book
+ *
+ */
 public class Evaluation {
 
+    //Properties
     String comment;
     double rate;
     User evaluater;
 
+    //Constructors
     public Evaluation(String comment, double rate, User evaluater)
     {
         this.comment = comment;
@@ -17,30 +25,60 @@ public class Evaluation {
     public Evaluation() {
     }
 
+    //Methods
+
+    /**
+     * Set method for comment
+     * @param comment is comment made by the user
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * Set method for rating
+     * @param rate is a rating (out of 5) given by the user
+     */
     public void setRate(double rate) {
         this.rate = rate;
     }
 
+    /**
+     * Set method for evaluater
+     * @param evaluater is the user evaluating the book
+     */
     public void setEvaluater(User evaluater) {
         this.evaluater = evaluater;
     }
 
+    /**
+     * Get method for comments
+     * @return returns the comment made by user
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * Get method for evaluater
+     * @return returns the user evaluating the book
+     */
     public User getEvaluater() {
         return evaluater;
     }
 
+    /**
+     * Get method for rate
+     * @return returns the rate given by the user
+     */
     public double getRate() {
         return rate;
     }
 
+    /**
+     * toString method for Evaluation
+     * @return returns the rating and the comment made by user as a string
+     */
     @Override
     public String toString() {
 
--- app/src/main/java/com/example/booked/models/Filterable.java
+++ app/src/main/java/com/example/booked/models/Filterable.java
@@ -1,7 +1,15 @@
 package com.example.booked.models;
 
+/**
+ * This is the interface Filterable. It allows the user to make filtered searches on Showroom
+ */
 public interface Filterable {
 
+    /**
+     *
+     * @param aUniversity
+     * @return
+     */
     Showroom filterByUniversity( String aUniversity);
 
     Showroom filterByCourse( String course);
